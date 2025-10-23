c219dbdecb0df4e29506bbc2530243dc02ab9241
TakoyakiNITEE <justinebautista080705@gmail.com>
2025-10-24 04:35:29 +0800

    Update admin dropdown link to include icon for better visibility

--- EHR_system_copy/header.php
+++ EHR_system_copy/header.php
@@ -329,7 +329,7 @@ require_once 'db.php';
                 </ul>
                 <ul class="navbar-nav">
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        
                             <i class="bi bi-person-circle me-1"></i>Admin
                         </a>
                         <ul class="dropdown-menu dropdown-menu-end">
